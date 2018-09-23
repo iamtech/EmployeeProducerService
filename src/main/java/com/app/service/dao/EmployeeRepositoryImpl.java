c3b9cfd08f23346031c17625fa95e901dc1b86b1
iamtech <iamtech4k@gmail.com>
2018-09-23 12:24:53 +0530

    working code

--- src/main/java/com/app/service/dao/EmployeeRepositoryImpl.java
+++ src/main/java/com/app/service/dao/EmployeeRepositoryImpl.java
@@ -15,11 +15,13 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
 	private Map<String, EmployeeBean> empData = new HashMap<String, EmployeeBean>();
 		
 		public EmployeeRepositoryImpl() {
-			EmployeeBean emp = new EmployeeBean("1000", "Ajit");
+			EmployeeBean emp = new EmployeeBean("1000", "Vincent");
 			empData.put("1000", emp);
-			emp = new EmployeeBean("2000", "Amay");
+			emp = new EmployeeBean("2000", "Harry");
 			empData.put("2000", emp);
-			emp = new EmployeeBean("3000", "Dinesh");
+			emp = new EmployeeBean("3000", "Clark");
+			empData.put("3000", emp);
+			emp = new EmployeeBean("4000", "Stephen");
 			empData.put("3000", emp);
 		}
 		
